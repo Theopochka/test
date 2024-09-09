0f154a3e4ce09b6219e1075d751268dc3b2665dd
Theopochka <theopka0@gmail.com>
2024-09-09 18:42:41 +0300

    Update autoupdate.lua

--- autoupdate.lua
+++ autoupdate.lua
@@ -12,7 +12,7 @@ encoding.default = 'CP1251'
 local u8 = encoding.UTF8
 
 local mvdPath = "autoupdate.lua"
-local mvdUrl = "https://raw.githubusercontent.com/DanielBagdasarian/MVD-Helper-Mobile/main/MVDHelper.lua"
+local mvdUrl = "https://raw.githubusercontent.com/Theopochka/test/main/autoupdate.lua"
 
 local function downloadFile(url, path)
 
@@ -24,49 +24,48 @@ local function downloadFile(url, path)
     headers = {
       ["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0;Win64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
 
-    },
-  }
+  },
+}
 
-  if status_code == 200 then
-    return true
+if status_code == 200 then
+  return true
+else
+  return false
+end
+end
+
+sampAddChatMessage("[Lavka Market]: Helper]: {FFFFFF} Проверка наличия обновлений...", 0x21D518)
+  local currentVersionFile = io.open(mvdPath, "r")
+  local currentVersion = currentVersionFile:read("*a")
+  currentVersionFile:close()
+  local response = http.request(mvdUrl)
+  if response and response ~= currentVersion then
+    sampAddChatMessage("[Lavka Market]: {FFFFFF} У вас не актуальная версия! Для обновления перейдите во вкладку Инфо", 0x21D518)
   else
-    return false
+    sampAddChatMessage("[Lavka Market]: {FFFFFF} Ó âàñ àêòóàëüíàÿ âåðñèÿ ñêðèïòà.", 0x21D518)
+  end
+  
+local function updateScript(scriptUrl, scriptPath)
+  sampAddChatMessage("[Lavka Market]: {FFFFFF} Ïðîâåðêà íàëè÷èÿ îáíîâëåíèé...", 0x21D518)
+local response = http.request(scriptUrl)
+  if response and response ~= currentVersion then
+      sampAddChatMessage("[Lavka Market]: {FFFFFF} Äîñòóïíà íîâàÿ âåðñèÿ ñêðèïòà! Îáíîâëåíèå...", 0x21D518)
+      
+      local success = downloadFile(scriptUrl, scriptPath)
+      if success then
+          sampAddChatMessage("[Lavka Market]: {FFFFFF} Ñêðèïò óñïåøíî îáíîâëåí.", 0x21D518)
+          thisScript():reload()
+      else
+          sampAddChatMessage("[Lavka Market]: {FFFFFF} Íå óäàëîñü îáíîâèòü ñêðèïò.", 0x21D518)
+      end
+  else
+      sampAddChatMessage("[Lavka Market]: {FFFFFF} Ñêðèïò óæå ÿâëÿåòñÿ ïîñëåäíåé âåðñèåé.", 0x21D518)
   end
 end
-
-    sampAddChatMessage("[Sashe4ka Police Helper]: {FFFFFF} Проверка наличия обновлений...", 0x8B00FF)
-    local currentVersionFile = io.open(mvdPath, "r")
-    local currentVersion = currentVersionFile:read("*a")
-    currentVersionFile:close()
-    local response = http.request(mvdUrl)
-    if response and response ~= currentVersion then
-    	sampAddChatMessage("[Sashe4ka Police Helper]: {FFFFFF} У вас не актуальная версия! Для обновления перейдите во вкладку Инфо", 0x8B00FF)
-    else
-    	sampAddChatMessage("[Sashe4ka Police Helper]: {FFFFFF} У вас актуальная версия скрипта.", 0x8B00FF)
-    end
-    
-local function updateScript(scriptUrl, scriptPath)
-    sampAddChatMessage("[Sashe4ka Police Helper]: {FFFFFF} Проверка наличия обновлений...", 0x8B00FF)
-	local response = http.request(scriptUrl)
-    if response and response ~= currentVersion then
-        sampAddChatMessage("[Sashe4ka Police Helper]: {FFFFFF} Доступна новая версия скрипта! Обновление...", 0x8B00FF)
-        
-        local success = downloadFile(scriptUrl, scriptPath)
-        if success then
-            sampAddChatMessage("[Sashe4ka Police Helper]: {FFFFFF} Скрипт успешно обновлен.", 0x8B00FF)
-            thisScript():reload()
-        else
-            sampAddChatMessage("[Sashe4ka Police Helper]: {FFFFFF} Не удалось обновить скрипт.", 0x8B00FF)
-        end
-    else
-        sampAddChatMessage("[Sashe4ka Police Helper]: {FFFFFF} Скрипт уже является последней версией.", 0x8B00FF)
-    end
-end
-
 function main()
     while not isSampAvailable() do wait(0) end
     while not sampIsLocalPlayerSpawned() do wait(0) end
-    sampAddChatMessage(u8:decode('Скрипт загружен'), -1)
+    sampAddChatMessage(u8:decode('РЎРєСЂРёРїС‚ Р·Р°РіСЂСѓР¶РµРЅ'), -1)
     
     sampRegisterChatCommand("up", updateScript)
     while true do
